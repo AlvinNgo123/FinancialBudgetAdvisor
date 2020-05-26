9857b4e8fa1127e94419913dc5922b1e0c2d4cf5
Alvin Ngo <aln011@ucsd.edu>
2020-05-25 17:29:05 -0700

    Finished implementing user desired saving amt

--- my_module/functions.py
+++ my_module/functions.py
@@ -13,7 +13,7 @@ def say_intro_get_name():
 	return name
 
 
-def is_input_valid(input_value):
+def is_number_valid(input_value):
 	if input_value.isnumeric():
 		return True
 	try:
@@ -42,16 +42,52 @@ def ask_for_current_spending(user_name):
 		'personal': '(cell phone, internet, etc)'
 	}
 
-	for category, spending_amt in current_spending:
+	for category, spending_amt in current_spending.items():
 		spending_amt = input('Please enter spending amt: ')
-		while (is_input_valid(spending_amt) == False):
+
+		while (is_number_valid(spending_amt) == False):
 			spending_amt = input("Sorry, not valid. Please enter spending amt: ")
 
+		current_spending[category] = spending_amt
+
+	return current_spending
+
+
+def calculate_total_spending(user_name, current_spending):
+	total_spending = 0
+	for category, spending_amt in current_spending.items():
+		total_spending += float(spending_amt)
+	total_spending = round(total_spending, 2)
+
+	print('Thank you, ' + user_name + '. From my calculations, it seems that your current spending totals to ' + str(total_spending))
+	return (total_spending)
+
+
+def get_user_saving(user_name, total_current_spending):
+	user_saving = input("How much do you want to save from your current spending amount?")
+	while (is_number_valid(user_saving) == False):
+		user_saving = input('Sorry ' + user_name + ", but that value wasn't valid. How much do you want to save? ")
+		try:
+			while(float(user_saving) > total_current_spending):
+				user_saving = input("I think you're trying to save more than you actually spend. Can you enter a value that's less than your current spend? ")
+		except:
+			continue
+
+	user_new_budget = round(float(total_current_spending) - float(user_saving), 2)
+	print("Perfect! Since your current spending is " + float(total_current_spending) + " and you want to save " + float(user_saving) + ',')
+	print("that means your new budget will total to " + user_new_budget + '!')
+	return user_saving
+
+def get_user_ranking():
+	pass 
 
 
 def start_program():
 	user_name = say_intro_get_name()
 	current_spending = ask_for_current_spending(user_name)
+	total_current_spending = calculate_total_spending(user_name, current_spending)
+	user_desired_saving = get_user_saving(user_name, total_current_spending)
+	user_category_ranking = get_user_ranking()
 
 #start_program()
 
