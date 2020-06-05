c3197ccc18e4ef47cfd89d5b048940aecef4f7d4
Alvin Ngo <aln011@ucsd.edu>
2020-06-05 07:49:35 -0700

    Figured out how to do one input at a time. Have 3 one input functions left to test

--- my_module/test_functions.py
+++ my_module/test_functions.py
@@ -1,49 +1,102 @@
 """Test for my functions.
-
-Note: because these are 'empty' functions (return None), here we just test
-  that the functions execute, and return None, as expected.
 """
+import unittest.mock as mock
+import functions as f
+##
+##
 
-from functions import is_number_valid, is_saving_valid, are_all_categories_listed, parse_user_ranking, start_program
-##
-##
+@mock.patch('functions.input', side_effect=['alvin', 'Ngo COGS', '18', ''])
+def test_say_intro_get_name(mock_choice):
+
+	name = f.say_intro_get_name()
+	assert name == 'alvin'
+
+	name = f.say_intro_get_name()
+	assert name == 'Ngo COGS'
+
+	name = f.say_intro_get_name()
+	assert name == '18'
+
+	name = f.say_intro_get_name()
+	assert name == ''
+	
 
 def test_is_number_valid():
 
-    assert is_number_valid('14') == True
-    assert is_number_valid('1234.34') == True
-    assert is_number_valid('130.21455') == True
-    assert is_number_valid('1.23.45') == False
-    assert is_number_valid('1one4') == False
-    assert is_number_valid('') == False
+	assert f.is_number_valid('14') == True
+	assert f.is_number_valid('1234.34') == True
+	assert f.is_number_valid('130.21455') == True
+	assert f.is_number_valid('1.23.45') == False
+	assert f.is_number_valid('1one4') == False
+	assert f.is_number_valid('') == False
+
+
+#def test_ask_for_current_spending: TODO
+
+
+def test_calculate_total_spending():
+
+	spendingDict1 = {
+		'food': '200',
+		'personal': '100',
+		'housing': '600'
+	}
+	assert f.calculate_total_spending('', spendingDict1) == 900.00
+
+	spendingDict2 = {
+		'food': '200.25',
+		'personal': '100.15',
+		'housing': '600.50'
+	}
+	assert f.calculate_total_spending('', spendingDict2) == 900.90
+
+	spendingDict3 = {
+		'food': '200.1543',
+		'personal': '100.4324',
+		'housing': '600.1642'
+	}
+	assert f.calculate_total_spending('', spendingDict3) == 900.75
 
 
 def test_is_saving_valid():
 
-	assert is_saving_valid('1') == True
-	assert is_saving_valid('2') == True
-	assert is_saving_valid('3') == True
-	assert is_saving_valid('4') == False
-	assert is_saving_valid('one') == False
-	assert is_saving_valid('') == False
+	assert f.is_saving_valid('1') == True
+	assert f.is_saving_valid('2') == True
+	assert f.is_saving_valid('3') == True
+	assert f.is_saving_valid('4') == False
+	assert f.is_saving_valid('one') == False
+	assert f.is_saving_valid('') == False
+
+
+@mock.patch('functions.input', side_effect=['1', '2', '3'])
+def test_get_user_saving(mock_choice):
+
+	saving_choice = f.get_user_saving('')
+	assert saving_choice == '1'
+
+	saving_choice = f.get_user_saving('')
+	assert saving_choice == '2'
+
+	saving_choice = f.get_user_saving('')
+	assert saving_choice == '3'
 
 
 def test_are_all_categories_listed():
 
-	assert are_all_categories_listed(['housing', 'utilities', 'food', 'transportation', 'entertainment', 'personal']) == True
-	assert are_all_categories_listed(['utilities', 'personal', 'entertainment', 'transportation', 'housing', 'food']) == True
-	assert are_all_categories_listed(['utilities', 'personal', 'entertainment', 'transportation']) == False
-	assert are_all_categories_listed(['utilities', 'personal', 'entertainment', 'transport', 'house', 'food']) == False
-	assert are_all_categories_listed([]) == False
+	assert f.are_all_categories_listed(['housing', 'utilities', 'food', 'transportation', 'entertainment', 'personal']) == True
+	assert f.are_all_categories_listed(['utilities', 'personal', 'entertainment', 'transportation', 'housing', 'food']) == True
+	assert f.are_all_categories_listed(['utilities', 'personal', 'entertainment', 'transportation']) == False
+	assert f.are_all_categories_listed(['utilities', 'personal', 'entertainment', 'transport', 'house', 'food']) == False
+	assert f.are_all_categories_listed([]) == False
 
 
 def test_parse_user_ranking():
 
-	assert parse_user_ranking('housing, utilities, food') == ['housing', 'utilities', 'food']
-	assert parse_user_ranking('housing,utilities,food') == ['housing', 'utilities', 'food']
-	assert parse_user_ranking('hoUsiNg, utIliTies,F ooD') == ['housing', 'utilities', 'food']
-	assert parse_user_ranking('a, b, cd') == ['a', 'b', 'cd']
-	assert parse_user_ranking('') == ['']
+	assert f.parse_user_ranking('housing, utilities, food') == ['housing', 'utilities', 'food']
+	assert f.parse_user_ranking('housing,utilities,food') == ['housing', 'utilities', 'food']
+	assert f.parse_user_ranking('hoUsiNg, utIliTies,F ooD') == ['housing', 'utilities', 'food']
+	assert f.parse_user_ranking('a, b, cd') == ['a', 'b', 'cd']
+	assert f.parse_user_ranking('') == ['']
 
 
 def test_start_program():
@@ -55,9 +108,9 @@ def test_start_program():
 
 
 
-    
+	
 
 
 
-                 
-    
+				 
+	
